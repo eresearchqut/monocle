96c1469ffd3ae6c5e4a7f8cdd59c1c0319e72a0b
ryan-bennett <ryan.bennett.au@gmail.com>
2021-09-17 12:43:25 +1000

    Added build action

--- packages/form-components/story/utils.ts
+++
@@ -1,5 +0,0 @@
-import {createAjv, JsonSchema, UISchemaElement} from "@jsonforms/core";
-
-export const initStore = (schema: JsonSchema, uischema: UISchemaElement, data?: any) => {
-    return { schema, uischema, data, ajv: createAjv() };
-};

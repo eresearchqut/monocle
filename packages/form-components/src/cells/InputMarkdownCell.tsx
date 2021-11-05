97c6296d21b67baab6b57c4fc9bedb27132f39da
ryan-bennett <ryan.bennett.au@gmail.com>
2021-11-05 15:44:47 +1000

    Fixed render issue with markdown editor

--- packages/form-components/src/cells/InputMarkdownCell.tsx
+++ packages/form-components/src/cells/InputMarkdownCell.tsx
@@ -1,16 +1,9 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {CellProps, optionIs, RankedTester, rankWith} from '@jsonforms/core';
 import {withJsonFormsCellProps} from '@jsonforms/react';
 
-import merge from 'lodash/merge';
-
 import "easymde/dist/easymde.min.css";
 import SimpleMdeReact from "react-simplemde-editor";
-
-export interface InputMarkdownCellOptions {
-    required?: boolean,
-    focus?: boolean
-}
 
 export const InputMarkdownCell = (props: CellProps) => {
     const {
@@ -18,27 +11,17 @@ export const InputMarkdownCell = (props: CellProps) => {
         id,
         path,
         handleChange,
-        config,
-        uischema,
         visible = true
     } = props;
-    const {focus, required} = merge({}, config, uischema.options) as InputMarkdownCellOptions;
-    const options = useMemo(() => {
-        return {
-            autofocus: focus
-        };
-    }, [uischema]);
+
     if (!visible) {
         return null;
     }
     return (
         <SimpleMdeReact
             id={id}
-            aria-required={required}
             value={data}
             onChange={(value) => handleChange(path, value)}
-            options={options}
-
         />
     );
 

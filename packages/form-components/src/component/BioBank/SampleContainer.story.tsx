620d2200ce634c2a15cc25f0be32d169e50fd41a
Tamas Szabo <szabtam@gmail.com>
2021-12-13 06:15:15 +0200

    SampleContainer - more cleanup

--- packages/form-components/src/component/BioBank/SampleContainer.story.tsx
+++ packages/form-components/src/component/BioBank/SampleContainer.story.tsx
@@ -58,11 +58,11 @@ FewSamples30By30.args = {
         { row: 13, col: 14, id: 'sample A' },
         { row: 13, col: 16, id: 'sample B' },
         { row: 15, col: 15, id: 'sample C', highlighted: true },
-        { row: 18, col: 14, id: 'sample D' },
-        { row: 18, col: 15, id: 'sample E' },
-        { row: 18, col: 16, id: 'sample F' },
-        { row: 17, col: 13, id: 'sample G' },
+        { row: 17, col: 13, id: 'sample D' },
         { row: 17, col: 17, id: 'sample H' },
+        { row: 18, col: 14, id: 'sample E' },
+        { row: 18, col: 15, id: 'sample F' },
+        { row: 18, col: 16, id: 'sample G' },
     ],
 };
 
@@ -72,7 +72,7 @@ OneSample6By2.args = {
         width: 6,
         length: 2,
     },
-    samples: [{ row: 0, col: 1, id: 'sample1' }],
+    samples: [{ row: 0, col: 1, id: 'sample 1' }],
 };
 
 export const OneSample10By1 = Template.bind({});
@@ -81,7 +81,7 @@ OneSample10By1.args = {
         width: 10,
         length: 1,
     },
-    samples: [{ row: 0, col: 1, id: 'sample1' }],
+    samples: [{ row: 0, col: 1, id: 'sample 1' }],
 };
 
 export const OneSample1By10 = Template.bind({});
@@ -90,7 +90,7 @@ OneSample1By10.args = {
         width: 1,
         length: 10,
     },
-    samples: [{ row: 1, col: 0, id: 'sample1' }],
+    samples: [{ row: 1, col: 0, id: 'sample 1' }],
 };
 
 // TODO - I'm not sure we should pass in the maxWidth like this
@@ -103,7 +103,7 @@ OneSample1By10WithMaxwidth.args = {
         length: 10,
     },
     maxWidth: '200px',
-    samples: [{ row: 1, col: 0, id: 'sample1' }],
+    samples: [{ row: 1, col: 0, id: 'sample 1' }],
 };
 
 export const Default = Template.bind({});
@@ -113,9 +113,14 @@ Default.args = {
         length: 10,
     },
     samples: [
-        { row: 0, col: 0, id: 'sample1' },
-        { row: 0, col: 9, id: 'sample2' },
-        { row: 2, col: 3, id: 'sample3', highlighted: true },
-        { row: 3, col: 2, id: 'sample4' },
+        { row: 0, col: 0, id: 'sample 1' },
+        { row: 0, col: 9, id: 'sample 2' },
+        { row: 2, col: 3, id: 'sample 3', highlighted: true },
+        { row: 2, col: 9, id: 'sample 4' },
+        { row: 3, col: 2, id: 'sample 5' },
+        { row: 3, col: 4, id: 'sample 6' },
+        { row: 5, col: 3, id: 'sample 7' },
+        { row: 9, col: 0, id: 'sample 8' },
+        { row: 9, col: 9, id: 'sample 9' },
     ],
 };

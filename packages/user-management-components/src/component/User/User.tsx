e3a9174350667a51ed7a3fe9c5a95ad3c95b0114
ryan-bennett <ryan.bennett.au@gmail.com>
2022-01-11 10:41:54 +1000

    ERE-1601: Added User Listing

--- packages/user-management-components/src/component/User/User.tsx
+++ packages/user-management-components/src/component/User/User.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent} from 'react';
+import startCase from 'lodash/startCase';
+import '../../style/definition-table.scss';
 
 export interface User {
     enabled: boolean
@@ -24,14 +26,26 @@ export interface UserProps extends StatePropsOfUser, HandlerPropsOfUser {
 
 export const User: FunctionComponent<UserProps> = (props) => {
 
-    const {username} = props.user;
+    const {username, enabled, status, created, lastModified, attributes} = props.user;
+    const attibuteKeys = Object.keys(attributes);
 
-    return <div className={'user-attributes'}>
+    const renderTable = (value: object) =>
         <dl>
-            <dt>Username</dt>
-            <dd>{username}</dd>
+            {Object.keys(value).map(key =>
+                <React.Fragment>
+                    <dt title={key}>{startCase(key)}</dt>
+                    <dd>{ typeof value[key] === 'object' ? renderTable(value[key]) : value[key]}</dd>
+                </React.Fragment>
+            )}
         </dl>
-    </div>;
+
+
+
+    return (
+        <div className={'definition-table'}>
+            {renderTable(props.user)}
+        </div>
+    );
 };
 
 export default User;

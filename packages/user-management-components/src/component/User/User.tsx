cc7ec51fd487a94e6d089528c0098519ce452bab
ryan-bennett <ryan.bennett.au@gmail.com>
2022-01-11 10:51:40 +1000

    ERE-1601: Refactored User Component

--- packages/user-management-components/src/component/User/User.tsx
+++ packages/user-management-components/src/component/User/User.tsx
@@ -1,5 +1,6 @@
 import React, {FunctionComponent} from 'react';
 import startCase from 'lodash/startCase';
+import {classNames} from 'primereact/utils';
 import '../../style/definition-table.scss';
 
 export interface User {
@@ -34,11 +35,24 @@ export const User: FunctionComponent<UserProps> = (props) => {
             {Object.keys(value).map(key =>
                 <React.Fragment>
                     <dt title={key}>{startCase(key)}</dt>
-                    <dd>{ typeof value[key] === 'object' ? renderTable(value[key]) : value[key]}</dd>
+                    <dd>{renderValue(value[key])}</dd>
                 </React.Fragment>
             )}
         </dl>
 
+    const renderValue = (value: any) => {
+        switch (typeof value) {
+            case 'object':
+                return renderTable(value);
+            case 'boolean':
+                return <i className={classNames('pi', {
+                    'true-icon pi-check-circle': value,
+                    'false-icon pi-times-circle': !value
+                })}/>;
+            default:
+                return value;
+        }
+    }
 
 
     return (

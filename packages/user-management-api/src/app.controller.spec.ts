4b8820f4b94e6cfc0c6c0e28b5826c0de6e8212c
ryan-bennett <ryan.bennett.au@gmail.com>
2022-01-31 09:37:22 +1000

    ERE-1594: Mock providers for tests

--- packages/user-management-api/src/app.controller.spec.ts
+++ packages/user-management-api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return health "UP"', () => {
+      expect(appController.health()).toBe('UP');
     });
   });
 });

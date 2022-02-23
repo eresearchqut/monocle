86c4821508161ed98853fc36b182e9e2a9007a41
ScriptSmith <ScriptSmith@users.noreply.github.com>
2022-02-23 17:48:01 +1000

    Use template type for resource identifier

--- packages/form-api/src/module/meta/utils.ts
+++ packages/form-api/src/module/meta/utils.ts
@@ -1,6 +1,2 @@
-/**
- * Build identifier string for resource id
- * @param resource
- * @param id
- */
-export const buildResourceIdentifier = (resource: string, id: string) => `Resource:${resource}#data:${id}`; // TODO: template string return type
+export const buildResourceIdentifier = (resource: string, id: string): `Resource:${string}#data:${string}` =>
+  `Resource:${resource}#data:${id}`;

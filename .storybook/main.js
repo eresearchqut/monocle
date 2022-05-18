b5d396fa600afd88b602215dcee28c3023fe3e80
ryan-bennett <ryan.bennett.au@gmail.com>
2022-05-18 16:10:00 +1000

    Replace lerna with turbo repo and changesets:
    * Use webpack 5 builder for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
 module.exports = {
+    core: {
+        builder: "webpack5",
+    },
     stories: ['../packages/**/src/**/*.story.mdx', '../packages/**/src/**/*.story.@(js|jsx|ts|tsx)'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         'storybook-stylesheet-toggle',
-        '@storybook/preset-scss',
-    ],
+        '@storybook/preset-scss'
+    ]
 };
